7867ba9aa04ed6b746eac8ad408daf54e4e67459
edwardyi <yi.wei.jie@hotmail.com>
2021-10-27 17:07:49 +0800

    fixed(Markdown): back slash for namespace

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,4 +3,4 @@
 Route::view('blogs', 'press::test'); // two collon for accessing package's test view
 
 Route::get('/test', 'TestController@index');
-// Route::get('/test', 'edwardyi/Press/Http/Controllers/TestController@index');
+// Route::get('/test', 'edwardyi\Press\Http\Controllers\TestController@index');
