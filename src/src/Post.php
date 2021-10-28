5b7ad58a2fc44c91452066c382b0473c3775fef2
edwardyi <yi.wei.jie@hotmail.com>
2021-10-28 09:58:51 +0800

    feat(Markdown): Allowing for Package Expansion

--- src/src/Post.php
+++ src/src/Post.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $guarded = [];
+
+    /**
+     * extra fields
+     */
+    public function extra($field)
+    {
+        return optional(json_decode($this->extra))->$field;
+    }
 }
--- src/src/Repositories/PostRepository.php
+++ src/src/Repositories/PostRepository.php
@@ -22,7 +22,25 @@ class PostRepository
             'slug' => Str::slug($post['title']),
             'title' => $post['title'],
             'body' => $post['body'],
-            'extra' => $post['extra'] ?? json_encode([])
+            'extra' => $this->extra($post)
         ]);
+    }
+
+    /**
+     * extra
+     * 
+     * @var array $post
+     * 
+     * @return string
+     */
+    protected function extra($post)
+    {
+        // if extra field not exits then use empty array as string
+        // $post['extra'] ?? json_encode([])
+        $extra = (array) json_decode($post['extra'] ?? '[]');
+
+        $attributes = array_except($extra, ['body', 'title', 'extra', 'identifier']);
+
+        return json_encode(array_merge($extra, $attributes));
     }
 }
