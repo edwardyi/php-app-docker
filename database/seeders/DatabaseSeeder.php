cb21d27cd6dc1702ef357c1426f69d6afaf98820
edwardyi <yi.wei.jie@hotmail.com>
2021-12-28 08:35:14 +0800

    feat(database): add customer, company, user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(UserSeeder::class);
+        $this->call(CompanySeeder::class);
+        $this->call(CustomerSeeder::class);
         // \App\Models\User::factory(10)->create();
     }
 }
